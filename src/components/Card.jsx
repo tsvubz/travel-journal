efc3254b70104adb247ca8f2926440b953052a00
Terence Svubure <tsvubure@gmail.com>
2024-07-05 14:03:05 +0200

    updated Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,30 +9,24 @@ function Card(props) {
   return (
     <div className='card-container'>
         <div className='card-image-container'>
-            <img src={`../../images/${props.img}`} alt="Mount Fuji" className='card-image'/>
+            <img src={`../../images/${props.item.imageUrl}`} alt="Mount Fuji" className='card-image'/>
         </div>
         <div className='card-info-container'>
             <div className='card-location'>
                 <span><FontAwesomeIcon icon={faLocationDot} style={{color: "#F55A5A"}}/> </span>
-                <span className='location'>{props.country}</span>
-                <span><a href={props.link} target='_blank' className='card-map'>View on Google Maps</a></span>
+                <span className='location'>{props.item.location}</span>
+                <span><a href={props.item.googleMapsUrl} target='_blank' className='card-map'>View on Google Maps</a></span>
             </div>
-            <h2 className='card-title'>{props.title}</h2>
-            <p className='card-date'><strong>{props.startDate} - {props.endDate}</strong></p>
-            <p className='card-text'>{props.text}</p>
+            <h2 className='card-title'>{props.item.title}</h2>
+            <p className='card-date'><strong>{props.item.startDate} - {props.item.endDate}</strong></p>
+            <p className='card-text'>{props.item.description}</p>
         </div>
     </div>
   )
 }
 
 Card.propTypes = {
-    img: PropTypes.string.isRequired,
-    country: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    startDate: PropTypes.string.isRequired,
-    endDate: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
+    item: PropTypes.string.isRequired,
 }
 
 export default Card
--- src/components/data.js
+++ src/components/data.js
@@ -1,11 +1,42 @@
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        id: 1,
+        title: "Etihad Stadium",
+        location: "POLAND",
+        googleMapsUrl: "https://maps.app.goo.gl/oSUYSWSdsvde6aaCA",
+        startDate: "12 Jan, 2024",
+        endDate: "18 Jan, 2024",
+        description: "The Tatra Mountains, part of the Carpathian mountain chain in eastern Europe, create a natural border between Slovakia and Poland. The Tatras are home to wildlife including the Tatra chamois, marmot, lynx and bears.",
+        imageUrl: "tatra.jpg"
+    },
+    {
+        id: 2,
+        title: "Eiffel Tower",
+        location: "ITALY",
+        googleMapsUrl: "https://maps.app.goo.gl/V6n1R2DfV7VNCWPd7",
+        startDate: "12 June, 2023",
+        endDate: "24 June, 2023",
+        description: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower from 1887 to 1889.",
+        imageUrl: "eiffel-tower.jpg"
+    },
+    {
+        id: 3,
+        title: "Victoria Falls",
+        location: "ZIMBABWE",
+        googleMapsUrl: "https://maps.app.goo.gl/uRGuVk4FBghBAVkn7",
+        startDate: "15 Dec, 2023",
+        endDate: "24 Dec, 2023",
+        description: "It is located on the border between Zambia and Zimbabwe and is one of the world's largest waterfalls, with a width of 1,708 m (5,604 ft)",
+        imageUrl: "victoria-falls.jpg"
+    },
+    {
+        id: 4,
+        title: "Kruger National Park",
+        location: "SOUTH AFRICA",
+        googleMapsUrl: "https://maps.app.goo.gl/7V7dsQTCRuaqXUGR8",
+        startDate: "10 Feb, 2024",
+        endDate: "16 Feb, 2024",
+        description: "Kruger National Park, in northeastern South Africa, is one of Africa’s largest game reserves. Its high density of wild animals includes the Big 5: lions, leopards, rhinos, elephants and buffalos.",
+        imageUrl: "kruger-national.jpg"
     }
 ]
